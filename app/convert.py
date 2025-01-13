ede09a6977cb37af0f518176ef9693760d4fe684
Čeněk Použník <potuznikcenek@gmail.com>
2025-01-13 22:00:51 +0100

    code reformatting - split function "convert" to 2 functions, use os.path.join where appropriate

--- app/convert.py
+++ app/convert.py
@@ -2,11 +2,59 @@ import os
 import os.path
 import shutil
 import subprocess
+from glob import glob
 
-def write_to_info_file(directory, message):
-    with open(f"instance/conversions/{directory}/info.txt", "w") as info_file:
+def write_to_info_file(conversion_id, message):
+    with open(f"instance/conversions/{conversion_id}/info.txt", "w") as info_file:
         info_file.write(message)
         info_file.flush()
+
+def preprocess_conversion(conversion_id) -> str:
+    """
+    a function for preprocessing conversions right after upload\n
+    expects a single .py or .zip file inside the conversion folder
+
+    if .zip file is present, it unpacks it and determines the root file\n
+    returns the root file that pyinstaller should be started on
+    """
+    conversion_directory = os.path.join("instance", "conversions", conversion_id)
+    files = os.listdir(conversion_directory)
+    if len(files) != 1:
+        raise Exception(f"Expected only one file in {conversion_directory}, found {len(files)}")
+    
+    write_to_info_file(conversion_id, "Analyzing file(s)\n")
+    filename = files[0]
+    if filename.endswith(".py"):
+        return filename
+    if not filename.endswith(".zip"):
+        raise Exception(f"Expected .py or .zip file, got {filename}")
+        
+    write_to_info_file(conversion_id, "Unpacking zip archive\n")
+    shutil.unpack_archive(os.path.join(conversion_directory, filename), conversion_directory, "zip")
+    os.remove(os.path.join(conversion_directory, filename)) # removed the zip file - no longer needed
+    dir_items = [f for f in os.listdir(conversion_directory) if f != "info.txt"]
+    if len(dir_items) == 1 and os.path.isdir(os.path.join(conversion_directory, dir_items[0])): # if there was only one item inside the zip and it is a folder
+        try:
+            # move everything from the subfolder to the root folder
+            for file in os.listdir(os.path.join(conversion_directory, dir_items[0])):
+                shutil.move(os.path.join(conversion_directory, dir_items[0], file), conversion_directory)
+        except Exception as _e:
+            pass
+    
+    python_files = [f for f in os.listdir(conversion_directory) if f.endswith(".py")]
+    if len(python_files) == 0:
+        write_to_info_file(conversion_id, f"Conversion failed: No python files found in your zip. Please rename the root file of your project to main.py or run.py and make sure it is located in the root of the zip archive (not in any folder)")
+        return False
+    if "run.py" in python_files:
+        return "run.py"
+    if "main.py" in python_files:
+        return "main.py"
+    if "app.py" in python_files:
+        return "app.py"
+    if len(python_files) == 1:
+        return python_files[0]
+    write_to_info_file(conversion_id, f"Conversion failed: Please rename the root file of your project to main.py or run.py and make sure it is located in the root of the zip archive (not in any folder)")
+    return False
 
 def create_venv(directory):
     initial_dir = os.getcwd()
@@ -50,56 +98,25 @@ def run_pyinstaller(directory, filename, venv: bool=False):
             _ = os.system(f"wine {initial_dir}/wine/drive_c/python3.11/python.exe -m PyInstaller {filename} --onedir")
     os.chdir(initial_dir)
 
-def convert(directory):
-    write_to_info_file(directory, "Analyzing file(s)\n")
-    for filename in os.listdir(f"instance/conversions/{directory}"): # expects only one file in instance/conversions/{directory} (either one py script or zip)
-        if filename.endswith(".py"):
-            root_file = filename
-            write_to_info_file(directory, f"Converting - {root_file}\n")
-            run_pyinstaller(directory, filename)
-            break
+def convert(conversion_id):
+    conversion_directory = os.path.join("instance", "conversions", conversion_id)
+    root_file = preprocess_conversion(conversion_id)
 
-        if filename.endswith(".zip"):
-            write_to_info_file(directory, "Unpacking zip archive\n")
-            shutil.unpack_archive(f"instance/conversions/{directory}/{filename}", f"instance/conversions/{directory}", "zip")
-            python_files = [f for f in os.listdir(f"instance/conversions/{directory}") if f.endswith(".py")]
-            root_file = None
-            if len(python_files) == 0:
-                dirs = [f.name for f in os.scandir(f"instance/conversions/{directory}") if f.is_dir()] # find all subfolders
-                if len(dirs) == 1: # if there is only one folder
-                    try:
-                        for file in os.listdir(f"instance/conversions/{directory}/{dirs[0]}"): # move everything from the subfolder to the root folder
-                            shutil.move(f"instance/conversions/{directory}/{dirs[0]}/{file}", f"instance/conversions/{directory}")
-                            python_files = [f for f in os.listdir(f"instance/conversions/{directory}") if f.endswith(".py")]
-                    except Exception as _e:
-                        pass
-                else:
-                    write_to_info_file(directory, f"Conversion failed: No python files found in your zip. Please rename the root file of your project to main.py or run.py and make sure it is located in the root of the zip archive (not in any folder)")
-                    return False
-            if len(python_files) == 1:
-                root_file = python_files[0]
-            if "app.py" in python_files:
-                root_file = "app.py"
-            if "main.py" in python_files:
-                root_file = "main.py"
-            if "run.py" in python_files:
-                root_file = "run.py"
-            if not root_file:
-                write_to_info_file(directory, f"Conversion failed: Please rename the root file of your project to main.py or run.py and make sure it is located in the root of the zip archive (not in any folder)")
-                return False
-            venv = False
-            if "requirements.txt" in os.listdir(f"instance/conversions/{directory}"):
-                write_to_info_file(directory, "Creating virtual environment")
-                create_venv(directory)
-                venv = True
-            write_to_info_file(directory, f"Converting - {root_file}\n")
-            run_pyinstaller(directory, root_file, venv=venv)
-            break
+    venv = False
+    if "requirements.txt" in os.listdir(conversion_directory):
+        write_to_info_file(conversion_id, "Creating virtual environment")
+        create_venv(conversion_id)
+        venv = True
+        
+    write_to_info_file(conversion_id, f"Converting - {root_file}\n")
+    run_pyinstaller(conversion_id, root_file, venv=venv)
 
-    if os.path.exists(f"instance/conversions/{directory}/dist/{filename.split(".")[0]}/{filename.split(".")[0]}.exe") or os.path.exists(f"instance/conversions/{directory}/dist/{root_file.split(".")[0]}/{root_file.split(".")[0]}.exe"):
-        write_to_info_file(directory, "Finshed conversion successfully\n")
-        write_to_info_file(directory, "Starting zip archive creation\n")
-        shutil.make_archive(f"instance/conversions/{directory}/output", "zip", f"instance/conversions/{directory}/dist")
-        write_to_info_file(directory, "Created zip archive - ready for download")
+    if glob(os.path.join(conversion_directory, "dist", "*", "*.exe")):
+        write_to_info_file(conversion_id, "Finshed conversion successfully\n")
+        write_to_info_file(conversion_id, "Starting zip archive creation\n")
+        shutil.make_archive(os.path.join(conversion_directory, "output"), "zip", os.path.join(conversion_directory, "dist"))
+        write_to_info_file(conversion_id, "Created zip archive - ready for download")
     else:
-        write_to_info_file(directory, "An error occured during conversion - please check that your python files run properly on python 3.11.9 and try again")
+        write_to_info_file(conversion_id, "An error occured during conversion - please check that your python files run properly on python 3.11.9 and try again")
+        return False
+    return True
